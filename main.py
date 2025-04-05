10f9535534e8cdc16861b994aecff3b1db0056db
stelour <stelourgg@gmail.com>
2025-04-05 20:57:40 +0500

    local ddos

---
+++ main.py
@@ -0,0 +1,18 @@
+import threading
+import src.args as args
+import src.simple_variant as simple_variant
+
+
+def main_work():
+    ip_arg , threads_number = args.parse_args()
+    thread_arr = []
+    for i in range(threads_number):
+        thread_arr.append(threading.Thread(target = simple_variant.odnop , args = (ip_arg , )))
+
+    for curr_thread in thread_arr:
+        curr_thread.start()
+
+if __name__ == "__main__":
+    main_work()
+
+# C:/Users/stelo/AppData/Local/Programs/Python/Python311/python.exe main.py --ip 194.213.117.129
